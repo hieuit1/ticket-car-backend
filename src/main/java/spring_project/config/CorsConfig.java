5c086ce5eabd0bc341f28de41b969dd7b68f5b12
xuanhieu12345 <hieuxuan213@gmail.com>
2025-07-23 23:14:45 +0700

    fix domain

--- src/main/java/spring_project/config/CorsConfig.java
+++ src/main/java/spring_project/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://ticketcar.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://ticket-car.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
